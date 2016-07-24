78d47158f3ea017eda50222bb078617e2a8aba34
HengYuZ <18819451621@sina.cn>
2016-07-24 17:47:59 +0800

    这是插入排序

---
+++ insertionSort.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghengyu
+ * Date: 2016/7/24
+ * Time: 17:22
+ * 插入排序
+ */
+function insertionSort(&$arr)
+{
+    $len = count($arr);
+    for ($i = 0; $i < $len; $i++) {
+        for ($j = $i; $j > 0; $j--) {
+            if ($arr[$j] < $arr[$j - 1]) {
+                $tmp = $arr[$j];
+                $arr[$j] = $arr[$j - 1];
+                $arr[$j - 1] = $tmp;
+            }
+        }
+    }
+}
+
+$arr = array(9, 2, 232, 12, 1, 3, 53, 17, 7);
+insertionSort($arr);
+var_dump($arr);
