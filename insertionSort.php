3e62cf9817627c438358f4ffd7d61ab07833745e
Kimxq <1448768191@qq.com>
2016-07-25 09:02:25 +0800

    insertionSort

---
+++ insertionSort.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanghengyu
+ * Date: 2016/7/22
+ * Time: 17:42
+ */
+
+function insertionSort(&$arr)
+{
+    $len = count($arr);
+    for ($i = 0; $i < $len; $i++) {
+        for ($j = 0; $j < $i; $j++) {
+            if($arr[$j]>$arr[$i]){
+                
+            }
+        }
+    }
+}
